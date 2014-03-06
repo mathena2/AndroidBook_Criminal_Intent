bee3505a542b256431cc9d72ab3b309c39f29cf6
mathena2 <stumpy6892@yahoo.com>
2014-03-06 10:57:02 -0500

    Criminal Intent app end of chapter 12

---
+++ CriminialIntent/gen/com/bignerdranch/android/criminialintent/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bignerdranch.android.criminialintent;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CriminialIntent/src/com/bignerdranch/android/criminialintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminialintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() 
+	{
+		return new CrimeListFragment();
+	}
+
+}
